60167a008d09db6c1ad42ca9f582f85816ddb13e
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-10-28 11:04:54 -0500

    merge

--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -6,7 +6,8 @@ import { useHistory } from "react-router"
 
 export const Post = () => {
     const { postId } = useParams()
-
+    const [newTag, setNewTag] = useState("")
+    const [tags, setTags] = useState([])
     const [post, setPost] = useState({})
     const [toDelete, setDelete] = useState(false)
     const history = useHistory()
@@ -18,6 +19,19 @@ export const Post = () => {
     const getPost = () => {
         return fetch(`http://localhost:8088/post/${postId}`)
     }
+
+    const getTags = () => {
+        const copy = { ...newTag }
+        copy.label = ""
+        setNewTag(copy)
+        fetch('http://127.0.0.1:8088/tags')
+            .then(res => res.json())
+            .then(tags => setTags(tags))
+    }
+
+    useEffect(() => {
+        getTags()
+    }, [])
 
     useEffect(() => {
         getPost()
@@ -35,6 +49,14 @@ export const Post = () => {
             <p>{post.content}</p>
             <h3>Posted: {post.publication_date}</h3>
             <p>By {post?.user?.first_name}</p>
+            <div className="form-group">
+                <p>Tags:</p>
+                {tags.map(t => (<>
+                    <label name="tag_id" value={t.id}>{t.label}</label>
+                        <input type="checkbox" name="tag_id" value={`${t.id}`}
+                        onChange={""}></input>
+                        </>))}
+            </div>
 
             {toDelete ?
                 <><p>Are you sure you wish to delete this post?</p>
